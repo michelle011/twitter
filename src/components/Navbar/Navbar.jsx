d5d82cd0b88d4a4499d5e8a7671650965aea3822
Michelle Fox-Romero <michelle.fox@michell-ltmdryt.internal.salesforce.com>
2022-06-22 13:12:35 -0700

    finished step 1

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,31 +1,37 @@
-import * as React from "react"
-import AvatarIcon from "../AvatarIcon/AvatarIcon"
-import "./Navbar.css"
+import * as React from "react";
+import AvatarIcon from "../AvatarIcon/AvatarIcon";
+import "./Navbar.css";
 
 export default function Navbar({ navLinks }) {
   return (
     <nav>
       <div className="navbar-container">
-        <NavLinks />
+        <NavLinks navLinks={navLinks} />
         <TwitterIcon />
         <SearchBar />
         <TweetButton />
       </div>
     </nav>
-  )
+  );
 }
 
 export function NavLinks({ navLinks }) {
-  return <ul className="nav-links">{/* WRITE CODE HERE */}</ul>
+  return (
+    <ul className="nav-links">
+      {navLinks.map((navLink, idx) => {
+        return <NavLink navLink={navLink} key={idx}></NavLink>;
+      })}
+    </ul>
+  );
 }
 
 export function NavLink({ navLink }) {
   return (
     <li className={navLink.className}>
       <i className={navLink.icon}></i>
-      <span></span>
+      <span>{navLink.label}</span>
     </li>
-  )
+  );
 }
 
 export function TwitterIcon() {
@@ -33,24 +39,22 @@ export function TwitterIcon() {
     <div className="twitter-icon">
       <i className="fab fa-twitter"></i>
     </div>
-  )
+  );
 }
-
 export function SearchBar() {
   return (
     <div className="search-bar">
       <input placeholder="Search Twitter" />
       <i className="fas fa-search"></i>
     </div>
-  )
+  );
 }
 
 export function TweetButton() {
   return (
     <div className="tweet-button">
       <AvatarIcon />
-
       <button>Tweet</button>
     </div>
-  )
+  );
 }
